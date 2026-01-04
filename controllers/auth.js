f9ac2131dc3aa7fa02ac044b43ffcd2a0d64d347
Nawaf-Aljar <nawaf.aljar@outlook.com>
2026-01-04 09:45:54 +0300

    Added sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,7 +22,7 @@ router.post("/sign-up", async (req, res) => {
             username: user.username,
             _id: user._id,
         }
-        const token = jwt.sign({payload}, process.env.JWT_SECRET)
+        const token = jwt.sign({ payload }, process.env.JWT_SECRET)
 
         res.status(201).json({ token })
 
@@ -31,5 +31,29 @@ router.post("/sign-up", async (req, res) => {
         console.log(err)
     }
 })
+router.post("/sign-in", async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const userInDatabase = await User.findOne({ username })
+        if (!userInDatabase) {
+            return res.status(401).json({ err: "Invalid username or password" });
+        }
+        const isValidPassword = bcrypt.compareSync(password, userInDatabase.password)
+        if (!isValidPassword) {
+            return res.status(401).json({ err: "Invalid username or password" });
+        }
+        const payload = {
+            username: userInDatabase.username,
+            _id: userInDatabase._id
+        }
+        const token = jwt.sign({ payload }, process.env.JWT_SECRET)
+        res.status(200).json({ token })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err: err.message })
+    }
+})
+
 
 module.exports = router
